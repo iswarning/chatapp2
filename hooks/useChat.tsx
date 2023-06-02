1e4fbc3979589a27659a6485b7ee032ef00c9563
Iswarning@1998 <vinhlc@itsj-group.com.vn>
2023-06-02 17:20:00 +0700

    update

---
+++ hooks/useChat.tsx
@@ -0,0 +1,29 @@
+import { db } from '@/firebase';
+import { createDataHook } from 'next-data-hooks';
+
+// this context is the GetStaticPropsContext from 'next'
+//                                                      👇
+const useBlogPost = createDataHook('ChatMessage', async (context) => {
+  const chatId = context.params?.id as string;
+
+  const ref = db.collection('chats').doc(chatId);
+    const messagesRes = await ref.collection('messages').orderBy('timestamp', 'asc').get();
+    const messages = messagesRes.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+    }))
+    .map((messages: any) => ({
+        ...messages,
+        timestamp: messages.timestamp.toDate().getTime(),
+    }));
+
+    const chatRes = await ref.get();
+    const chat = {
+        id: chatRes.id,
+        ...chatRes.data()
+    };
+
+    return blogPost;
+    });
+
+export default useBlogPost;
