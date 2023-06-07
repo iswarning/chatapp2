f62474d28ac8d6713c832226517eebd720854d60
iswarning <iswarning@gmail.com>
2023-06-07 21:11:40 +0700

    up

--- components/ProfilePage/ShowStatusFriend/ShowStatusFriend.tsx
+++ components/ProfilePage/ShowStatusFriend/ShowStatusFriend.tsx
@@ -4,24 +4,13 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import firebase from "firebase";
 import { io } from "socket.io-client";
 import sendNotificationFCM from "@/utils/sendNotificationFCM";
+import { useRouter } from "next/router";
 
 export default function ShowStatusFriend({userInfo, statusFriend}: any) {
     
     const [user] = useAuthState(auth);
     const [status, setStatus] = useState(statusFriend);
-    const socket = io(process.env.NEXT_PUBLIC_SOCKET_IO_URL!);
-
-    useEffect(() => {
-        socket.on("response-notify", (msg: any) => {
-            const data = JSON.parse(msg);
-            if(data.type === 'accept-friend' && data.recipient === user?.email) {
-                setStatus('isFriend');
-            }
-          });
-          return () => {
-            socket.disconnect()
-          }
-    },[])
+    const router = useRouter();
 
     const onAddFriend = async() => {
         await db
@@ -31,13 +20,6 @@ export default function ShowStatusFriend({userInfo, statusFriend}: any) {
             recipientEmail: userInfo?.email!,
             createdAt: firebase.firestore.FieldValue.serverTimestamp()
         })
-        // socket.emit("send-notify", JSON.stringify({
-        //     sender: user?.email,
-        //     recipient: userInfo?.email,
-        //     name: user?.displayName,
-        //     createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-        //     type: 'send-add-friend'
-        // }))
         setStatus('isFriendRequest');
         sendNotificationFCM(
             'Notification',
@@ -92,7 +74,7 @@ export default function ShowStatusFriend({userInfo, statusFriend}: any) {
                             <button 
                                 className="block mx-2 rounded-full hover:shadow-lg font-semibold text-black px-6 py-2" 
                                 style={{border: '1px solid'}}
-                                onClick={onAddFriend}>Send Message</button>
+                                onClick={() => router.push(`/chat?id=${}`)}>Send Message</button>
                         </div>
             case 'isFriendRequest':
                 return <div className="flex justify-center py-4 lg:pt-4 pt-8">
