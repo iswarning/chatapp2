02388a6cfc1d4383bacf8aa8100027aac40df9a3
iswarning <iswarning@gmail.com>
2023-07-05 21:45:32 +0700

    update

--- components/ChatScreen/InfoContentScreen.tsx
+++ components/ChatScreen/InfoContentScreen.tsx
@@ -14,6 +14,7 @@ import MemberInChat from '../MemberInChat'
 import MemberElement from '../MemberElement'
 import FriendElement from '../FriendElement'
 import { selectChatState } from '@/redux/chatSlice'
+import { selectFriendState } from '@/redux/friendSlice'
 export default function InfoContentScreen() {
 
     const [user] = useAuthState(auth)
@@ -23,17 +24,13 @@ export default function InfoContentScreen() {
     const [showCustom, setShowCustom] = useState(false)
     const [showMember, setShowMember] = useState(false)
 
-    const [recipientSnapshot] = useCollection(
-        db.collection("users").where("email",'==', getRecipientEmail(chatState.currentChat.users, appState.userInfo))
-    )
-
     const getRecipientAvatar = () => {
         if (chatState.currentChat?.isGroup) {
           if (chatState.currentChat?.photoURL.length > 0) return chatState.currentChat?.photoURL;
           else return "/images/group-default.jpg";
         } else {
-          let photoUrl = recipientSnapshot?.docs?.[0]?.data().photoURL;
-          if (photoUrl?.length > 0) return photoUrl;
+          let photoUrl = chatState.currentChat.recipientInfo?.photoURL;
+          if (photoUrl?.length! > 0) return photoUrl;
           else return "/images/avatar-default.png";
         }
     };
@@ -46,10 +43,10 @@ export default function InfoContentScreen() {
                         <a href="javascript:;" className="text-gray-600 tooltip w-8 h-8 block flex items-center justify-center absolute top-0 right-0 mr-1 mt-1"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="feather feather-edit-2 w-4 h-4"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg> </a>
                             <div className="w-20 h-20 mx-auto image-fit">
                                 {
-                                    getRecipientAvatar() ? <Image src={getRecipientAvatar()} width={100} height={100} alt='' className='rounded-full' /> : null
+                                    getRecipientAvatar() ? <Image src={getRecipientAvatar()!} width={100} height={100} alt='' className='rounded-full' /> : null
                                 }
                             </div>
-                        <div className="text-base font-medium text-center mt-3">{ chatState.currentChat.isGroup ? "Group: " + chatState.currentChat.name : recipientSnapshot?.docs?.[0]?.data().fullName}</div>
+                        <div className="text-base font-medium text-center mt-3">{ chatState.currentChat.isGroup ? "Group: " + chatState.currentChat.name : chatState.currentChat.recipientInfo?.fullName}</div>
                     </div>
                     {/* <div className="intro-y h-full box p-4">
                         <div className="text-base font-medium cursor-pointer" onClick={() => setShowSharedFile(!showSharedFile)}>Shared Files { showSharedFile ? <ArrowDropDownIcon fontSize='small' /> : <ArrowDropUpIcon fontSize='small' />}</div>
--- components/MemberInChat.tsx
+++ components/MemberInChat.tsx
@@ -1,34 +1,56 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import { useCollection } from 'react-firebase-hooks/firestore'
 import { db } from '@/firebase'
-import { MapUserData } from '@/types/UserType'
+import { MapUserData, UserType } from '@/types/UserType'
 import {useDispatch,useSelector} from 'react-redux'
 import { selectAppState } from '@/redux/appSlice'
 import styled from 'styled-components'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import { selectChatState } from '@/redux/chatSlice'
+import { selectFriendState } from '@/redux/friendSlice'
 export default function MemberInChat({email}: {email: string}) {
   const [showAction, setShowAction] = useState(false)
-  const [userInfo] = useCollection(
-    db
-    .collection("users")
-    .where("email",'==',email)
-    .limit(1)
-  )
-
-  const info = MapUserData(userInfo?.docs?.[0]!)
+  // const [userInfo] = useCollection(
+  //   db
+  //   .collection("users")
+  //   .where("email",'==',email)
+  //   .limit(1)
+  // )
+  const [user,setUser] = useState<UserType>()
 
   const appState = useSelector(selectAppState)
   const chatState = useSelector(selectChatState)
+  const friendState = useSelector(selectFriendState)
+
+  const info = friendState.listFriend.find((friend) => friend.userInfo?.email === email)?.userInfo
+
+  useEffect(() => {
+    if (info) {
+      setUser(info)
+    } else {
+      getUserInfo().then((userInfo) => setUser(userInfo))
+    }
+  },[])
+
+  const getUserInfo = async() => {
+    return MapUserData(
+      (await db
+        .collection("users")
+        .where("email",'==',email)
+        .limit(0)
+        .get())
+        .docs?.[0]
+    )
+  }
 
   return (
     <div className="shared-file border-gray-200 dark:border-dark-5 flex items-center p-3 border rounded-md mt-3">
-        <Image src={info.photoURL} width={40} height={40} alt='' className='rounded-full'/>
+        <Image src={user?.photoURL!} width={40} height={40} alt='' className='rounded-full'/>
         <div className="w-full ml-3">
-            <div className="text-gray-700 dark:text-gray-300 whitespace-nowrap font-medium truncate">{info.fullName}</div>
-            <div className="text-gray-600 whitespace-nowrap text-xs mt-0.5">{info.email}</div>
+            <div className="text-gray-700 dark:text-gray-300 whitespace-nowrap font-medium truncate">{user?.fullName}</div>
+            <div className="text-gray-600 whitespace-nowrap text-xs mt-0.5">{user?.email}</div>
         </div>
         <div className="">
             {/* <a onClick={() => dispatch(setAppState({ data: !appState.AppState.showDropdownActionUser, type: "App/DropdownActionUser" }))} className="dropdown-toggle w-4 h-4" href="javascript:;" aria-expanded="false"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="feather feather-more-vertical w-4 h-4"><circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="5" r="1"></circle><circle cx="12" cy="19" r="1"></circle></svg> </a> */}
